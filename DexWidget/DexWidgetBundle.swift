5d19593ab3cca868f4f0e56790d1d8dec36214e7
Sarvesh Roshan <lalr.466@gamil.com>
2025-11-21 23:47:54 +0530

    Add widget extension

---
+++ DexWidget/DexWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  DexWidgetBundle.swift
+//  DexWidget
+//
+//  Created by Sarvesh Roshan on 21/11/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
